58eb1666aba41bf6197dc221118764bf744fa24e
Violet-coco <18227217142@163.com>
2019-05-10 19:28:10 +0800

    答辩打分

---
+++ app/src/main/java/com/manage_system/ui/manage/activity/teacher/TeacherScoreDetailActivity.java
@@ -0,0 +1,196 @@
+package com.manage_system.ui.manage.activity.teacher;
+
+import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.manage_system.R;
+import com.manage_system.net.ApiConstants;
+import com.manage_system.ui.personal.GuideStudentInfoActivity;
+import com.manage_system.utils.DownloadUtil;
+import com.manage_system.utils.OkManager;
+import com.manage_system.utils.OpenFileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import okhttp3.Call;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class TeacherScoreDetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.pName)
+    EditText pName;
+    @BindView(R.id.stu_name)
+    EditText stu_name;
+    @BindView(R.id.stu_id)
+    EditText stu_id;
+    @BindView(R.id.guide_teacher_name)
+    EditText guide_teacher_name;
+    @BindView(R.id.scoreWorth)
+    EditText scoreWorth;
+    @BindView(R.id.scoreReport)
+    EditText scoreReport;
+    @BindView(R.id.scoreProcess)
+    EditText scoreProcess;
+    @BindView(R.id.scoreDefence)
+    EditText scoreDefence;
+    @BindView(R.id.comment)
+    EditText comment;
+    @BindView(R.id.ct_item_submit)
+    Button ct_item_submit;
+    private static String TAG = "选题界面";
+    private String sid,ts_scoreWorth,ts_scoreReport,ts_scoreProcess,ts_scoreDefence,ts_comment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ms_teacher_reply_plan_score_detail);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+
+        SharedPreferences sp=getSharedPreferences("processData", MODE_PRIVATE);
+        JSONObject obj = JSON.parseObject(sp.getString("showDefStudents" , ""));
+        Log.w(TAG,obj.toString());
+        JSONArray array = new JSONArray(obj.getJSONArray("studentList"));
+        Log.w(TAG,array.toString());
+        Log.w(TAG,"position==="+intent.getStringExtra("position"));
+
+        JSONObject object = array.getJSONObject(Integer.parseInt(intent.getStringExtra("position")));
+        JSONObject defScore = object.getJSONObject("defScore");
+
+        Log.e(TAG,defScore.toString());
+        stu_name.setText(object.getString("name"));
+        stu_id.setText(object.getString("identifier"));
+        pName.setText(object.getString("pName"));
+        guide_teacher_name.setText(object.getJSONObject("gt").getString("name"));
+
+    }
+
+    /**启动这个Activity的Intent
+     * @param context
+     * @return
+     */
+    public static Intent createIntent(Context context) {
+        return new Intent(context, TeacherScoreDetailActivity.class);
+    }
+
+    @OnClick({R.id.iv_back,R.id.ct_item_submit})
+    public void onClick(View v) {//直接调用不会显示v被点击效果
+        switch (v.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.ct_item_submit:
+                showDialog();
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void submitData() {
+        sid = stu_id.getText().toString().trim();
+        ts_scoreWorth=scoreWorth.getText().toString().trim();
+        ts_scoreReport=scoreReport.getText().toString().trim();
+        ts_scoreProcess=scoreProcess.getText().toString().trim();
+        ts_scoreDefence=scoreDefence.getText().toString().trim();
+        ts_comment=comment.getText().toString().trim();
+
+        OkManager manager = OkManager.getInstance();
+        // 接口名称
+        String api;
+        MultipartBody.Builder builder = new MultipartBody.Builder();
+        builder.setType(MultipartBody.FORM)
+                .addFormDataPart("sid", sid)
+                .addFormDataPart("scoreWorth", ts_scoreWorth)
+                .addFormDataPart("scoreReport", ts_scoreReport)
+                .addFormDataPart("scoreProcess", ts_scoreProcess)
+                .addFormDataPart("scoreDefence", ts_scoreDefence)
+                .addFormDataPart("comment", ts_comment);
+
+        RequestBody requestBody = builder.build();
+
+        manager.postFile(ApiConstants.teacherApi + "/scoringDefence", requestBody,new okhttp3.Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "onFailure: ",e);
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseBody = response.body().string();
+                Log.e(TAG,responseBody);
+                final JSONObject obj = JSON.parseObject(responseBody);
+                Log.e(TAG,obj.toString());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(obj.get("statusCode").equals(100)){
+                            Toast.makeText(TeacherScoreDetailActivity.this, obj.getString("msg"), Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(TeacherScoreDetailActivity.this,TeacherScoreMainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }else {
+                            Toast.makeText(TeacherScoreDetailActivity.this, obj.getString("msg"), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+    public void showDialog(){
+        final Dialog dialog = new Dialog(TeacherScoreDetailActivity.this, R.style.MyDialog);
+        //设置它的ContentView
+        dialog.setContentView(R.layout.alert_dialog);
+        ((TextView)dialog.findViewById(R.id.tvAlertDialogTitle)).setText("确认提交");
+        ((TextView)dialog.findViewById(R.id.tvAlertDialogMessage)).setText("确认提交分数？");
+        dialog.show();
+
+        Button confirm = (Button)dialog.findViewById(R.id.btnAlertDialogPositive);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+                submitData();
+            }
+        });
+        Button cancel = (Button) dialog.findViewById(R.id.btnAlertDialogNegative);
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                dialog.dismiss();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/manage_system/ui/manage/activity/teacher/TeacherScoreMainActivity.java
@@ -0,0 +1,157 @@
+package com.manage_system.ui.manage.activity.teacher;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.manage_system.R;
+import com.manage_system.net.ApiConstants;
+import com.manage_system.ui.manage.adapter.MyAdapter;
+import com.manage_system.utils.OkManager;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import okhttp3.Call;
+import okhttp3.Response;
+
+public class TeacherScoreMainActivity extends AppCompatActivity {
+
+    @BindView(R.id.recycler)
+    RecyclerView recycleView;
+    @BindView(R.id.tool_bar)
+    RelativeLayout tool_bar;
+    @BindView(R.id.iv_back)
+    ImageButton iv_back;
+    @BindView(R.id.top_title)
+    TextView top_title;
+
+    public List<Map<String,Object>> list=new ArrayList<>();
+
+    private static String TAG = "已出题界面";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_tab);
+        ButterKnife.bind(this);
+        tool_bar.setVisibility(View.VISIBLE);
+        top_title.setText("答辩打分");
+        initData();
+    }
+
+    /**启动这个Activity的Intent
+     * @param context
+     * @return
+     */
+    public static Intent createIntent(Context context) {
+        return new Intent(context, TeacherScoreMainActivity.class);
+    }
+
+    public void initData() {
+        OkManager manager = OkManager.getInstance();
+        Map<String, String> map = new HashMap<String, String>();
+        manager.post(ApiConstants.teacherApi + "/showDefStudents", map,new okhttp3.Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.e(TAG, "onFailure: ",e);
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                final String responseBody = response.body().string();
+                Log.e(TAG,responseBody);
+                final JSONObject obj = JSON.parseObject(responseBody);
+                final String msg = obj.getString("msg");
+                Log.e(TAG,responseBody);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(obj.get("statusCode").equals(100)){
+                            final JSONArray array = new JSONArray(obj.getJSONObject("data").getJSONArray("studentList"));
+                            SharedPreferences sp=getSharedPreferences("processData", MODE_PRIVATE);
+                            SharedPreferences.Editor editor=sp.edit();
+                            editor.putString("showDefStudents",obj.getJSONObject("data").toString());
+                            editor.commit();
+                            String score;
+                            for (int i = 0; i < array.size(); i++) {
+                                JSONObject object = array.getJSONObject(i);
+                                Map<String, Object> map = new HashMap<>();
+                                map.put("identifier", object.getString("identifier"));
+                                map.put("name", object.getString("name"));
+                                map.put("pName", object.getString("pName"));
+                                map.put("gt_identifier", object.getJSONObject("gt").getString("id"));
+                                map.put("gt_name", object.getJSONObject("gt").getString("name"));
+                                if(object.getJSONObject("defScore").getBooleanValue("hasScored")){
+                                    score = object.getJSONObject("defScore").getString("scoreTotal");
+                                }else{
+                                    score = "未打分";
+                                }
+                                map.put("scoreTotal", score);
+                                list.add(map);
+                            }
+                            recycleView.setLayoutManager(new LinearLayoutManager(TeacherScoreMainActivity.this,LinearLayoutManager.VERTICAL,false));
+//                            设置适配器
+                            MyAdapter adapter = new MyAdapter(TeacherScoreMainActivity.this,list,"2005");
+                            adapter.setOnItemClickListener(new MyAdapter.OnItemClickListener() {
+                                @Override
+                                public void onItemClick(int position) {
+                                    // 这里本来是跳转页面 ，我们就在这里直接让其弹toast来演示
+                                    Log.w(TAG,"位置是："+position);
+                                    if(array.getJSONObject(position).getJSONObject("defScore").getBooleanValue("hasScored")){
+                                        Intent intent = new Intent(TeacherScoreMainActivity.this,TeacherScoreActivity.class);
+                                        intent.putExtra("position",position + "");
+                                        startActivity(intent);
+                                    }else{
+                                        Intent intent = new Intent(TeacherScoreMainActivity.this,TeacherScoreDetailActivity.class);
+                                        intent.putExtra("position",position + "");
+                                        startActivity(intent);
+                                    }
+//                                    Intent intent = new Intent(TeacherScoreMainActivity.this,TeacherScoreActivity.class);
+//                                    intent.putExtra("position",position + "");
+//                                    startActivity(intent);
+
+                                }
+                            });
+                            recycleView.setAdapter(adapter);
+
+                        }else{
+                            Toast.makeText(TeacherScoreMainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
+        });
+    }
+
+    @OnClick(R.id.iv_back)
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.iv_back:
+                finish();
+                break;
+            default:
+                break;
+        }
+    }
+
+}
